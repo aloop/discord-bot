20ee6bdd1413d5ea709191594b7c64b4a557f2bb
Anthony Loop <anthony@anthonyloop.com>
2022-06-17 17:52:55 -0700

    Remove axios in favor of fetch

--- commands/wow-token.js
+++ commands/wow-token.js
@@ -1,11 +1,11 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const axios = require("axios").default;
 
 const { token } = require("../store.js");
 
 const { blizzard } = require("../config.js")();
 
-const authTokenUrl = "https://us.battle.net/oauth/token";
+const authTokenUrl =
+    "https://us.battle.net/oauth/token?grant_type=client_credentials";
 const tokenPriceUrl =
     "https://us.api.blizzard.com/data/wow/token/index?namespace=dynamic-us";
 
@@ -15,33 +15,36 @@ async function getAuthToken() {
         return token.accessToken;
     }
 
-    const auth_response = await axios({
+    const credentials = Buffer.from(
+        `${blizzard.client_id}:${blizzard.client_secret}`
+    ).toString("base64");
+
+    const auth_response = await fetch(authTokenUrl, {
         method: "POST",
-        url: authTokenUrl,
-        auth: {
-            username: blizzard.client_id,
-            password: blizzard.client_secret,
+        headers: {
+            Authorization: `Basic ${credentials}`,
         },
-        data: "grant_type=client_credentials",
     });
 
-    token.expiresAt = Date.now() + parseInt(auth_response.data.expires_in, 10);
-    token.accessToken = auth_response.data.access_token;
+    const data = await auth_response.json();
+
+    token.expiresAt = Date.now() + parseInt(data.expires_in, 10);
+    token.accessToken = data.access_token;
 
     return token.accessToken;
 }
 
 async function getTokenPrice() {
-    const response = await axios({
-        method: "GET",
-        url: tokenPriceUrl,
+    const response = await fetch(tokenPriceUrl, {
         headers: {
             Authorization: `Bearer ${await getAuthToken()}`,
         },
     });
 
+    const data = await response.json();
+
     // Price is in copper, convert to gold
-    return response.data.price / 100 / 100;
+    return data.price / 100 / 100;
 }
 
 module.exports = {
