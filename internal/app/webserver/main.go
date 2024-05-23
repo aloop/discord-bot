9f9562eb0c770a93522c0a6733ae9996b58ca27e
Anthony Loop <anthony@aloop.dev>
2024-05-22 20:19:57 -0700

    allow http server to listen on a unix socket

--- internal/app/webserver/main.go
+++ internal/app/webserver/main.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/aloop/discord-bot/internal/app/blizzard"
 	"github.com/aloop/discord-bot/internal/pkg/config"
@@ -16,6 +19,13 @@ type handlerData struct {
 }
 
 func Run(ctx context.Context, b *blizzard.BlizzardClient, c *config.Config) error {
+	var (
+		listener     net.Listener
+		err          error
+		isUnixSocket bool
+		socketPath   string
+	)
+
 	h := &handlerData{
 		blizzard: b,
 	}
@@ -24,9 +34,25 @@ func Run(ctx context.Context, b *blizzard.BlizzardClient, c *config.Config) erro
 
 	mux.HandleFunc("GET /wow-token/chart/{unit}/{period}", h.handleChartRequest)
 
+	if strings.HasPrefix(c.HTTP.ListenHost, "unix:") {
+		isUnixSocket = true
+		socketPath = strings.TrimPrefix(c.HTTP.ListenHost, "unix:")
+
+		listener, err = createUnixSocketListener(socketPath)
+		if err != nil {
+			return fmt.Errorf("error creating unix socket listener: %v", err)
+		}
+	} else {
+		addr := fmt.Sprintf("%s:%d", c.HTTP.ListenHost, c.HTTP.ListenPort)
+		listener, err = net.Listen("tcp", addr)
+		if err != nil {
+			return fmt.Errorf("error creating tcp listener: %v", err)
+		}
+	}
+
 	log.Println("Starting HTTP server")
 	go func() {
-		if err := http.ListenAndServe(fmt.Sprintf("%s:%d", c.HTTP.ListenHost, c.HTTP.ListenPort), mux); err != nil {
+		if err := http.Serve(listener, mux); err != nil {
 			log.Panicf(
 				"Failed to start HTTP server on %s:%d\n\n%v",
 				c.HTTP.ListenHost,
@@ -36,9 +62,36 @@ func Run(ctx context.Context, b *blizzard.BlizzardClient, c *config.Config) erro
 		}
 	}()
 
-	log.Printf("HTTP server started on %s:%d", c.HTTP.ListenHost, c.HTTP.ListenPort)
+	if isUnixSocket {
+		log.Printf("HTTP server started on unix socket at %s", socketPath)
+	} else {
+		log.Printf("HTTP server started on %s:%d", c.HTTP.ListenHost, c.HTTP.ListenPort)
+	}
 
 	return nil
+}
+
+func createUnixSocketListener(socketPath string) (net.Listener, error) {
+	// Attempt to remove the socket if it already exists
+	if err := os.Remove(socketPath); err != nil && !os.IsNotExist(err) {
+		return nil, fmt.Errorf("could not remove existing socket at \"%s\": %v", socketPath, err)
+	}
+
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"could not create a listener for the unix socket at %s: %v",
+			socketPath,
+			err,
+		)
+	}
+
+	// TODO: add config options to set socket permissions
+	if err := os.Chmod(socketPath, 0640); err != nil {
+		return nil, fmt.Errorf("failed to set unix socket permissions: %v", err)
+	}
+
+	return listener, nil
 }
 
 func (h *handlerData) handleChartRequest(w http.ResponseWriter, req *http.Request) {
@@ -74,14 +127,8 @@ func (h *handlerData) handleChartRequest(w http.ResponseWriter, req *http.Reques
 
 	img, err := h.blizzard.GeneratePriceChart(unit, int(period))
 	if err != nil {
-		log.Println(err)
-
-		w.WriteHeader(http.StatusInternalServerError)
-		_, err := w.Write([]byte("500 Internal Server Error"))
-		if err != nil {
-			log.Printf("Failed to write HTTP error response\n%v\n", err)
-		}
-
+		log.Printf("failed to generate price chart for request: %v", err)
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
